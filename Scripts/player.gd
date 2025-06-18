fa80d754d1083930777cd3a3e47f471334615e3b
chenkjia <chenkjia@live.com>
2025-06-18 21:54:28 +0800

    initgame

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,12 +2,15 @@ extends CharacterBody2D
 
 @export var move_speed: float = 50
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@export var animator: AnimatedSprite2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("left","right","up","down") * move_speed
+	# 播放动画代码
+	if velocity == Vector2.ZERO:
+		animator.play('idle')
+	else:
+		animator.play('run')
 	move_and_slide()
