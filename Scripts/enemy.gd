44d058f18a2f589dbb2fd95871eda876972ae562
chenkjia <chenkjia@live.com>
2025-06-21 18:19:47 +0800

    initgame

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D:
+	if body is CharacterBody2D and not is_dead:
 		body.game_over()
 	pass # Replace with function body.
 
@@ -22,6 +22,7 @@ func _on_area_entered(area: Area2D) -> void:
 		$AnimatedSprite2D.play("death")
 		area.queue_free()
 		get_tree().current_scene.score += 1
+		$DeadSound.play()
 		await get_tree().create_timer(0.6).timeout
 		queue_free()
 	pass # Replace with function body.
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,6 +7,13 @@ extends CharacterBody2D
 @export var is_game_over: bool = false
 @export var bullet_scene: PackedScene
 
+func _process(delta: float) -> void:
+	if velocity == Vector2.ZERO or is_game_over:
+		$RunningSound.stop()
+	elif not $RunningSound.playing:
+		$RunningSound.play()
+		
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -23,17 +30,21 @@ func game_over():
 	if not is_game_over:
 		is_game_over = true
 		animator.play('game_over')
-		
 		get_tree().current_scene.show_game_over()
-		
-		await get_tree().create_timer(3).timeout
-		get_tree().reload_current_scene()
-
+		$GameOverSound.play()
+		$RestartTimer.start()
 
 func _on_fire() -> void:
 	if velocity != Vector2.ZERO or is_game_over:
-		return 
+		return
+	$FireSound.play()
 	var bullet_node = bullet_scene.instantiate()
 	bullet_node.position = position + Vector2(6,6)
 	get_tree().current_scene.add_child(bullet_node)
 	pass # Replace with function body.
+	
+
+
+
+func _reload_scene() -> void:
+	get_tree().reload_current_scene()
